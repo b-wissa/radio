3a9a06be49263dd378c7627f4ac188fe34717fd4
Bassem Wissa <b.r.wissa@gmail.com>
2024-11-21 14:32:42 +0100

    Add Storage comment

--- app/src/main/java/com/challenge/radio/station/repository/StationsRepositoryImpl.kt
+++ app/src/main/java/com/challenge/radio/station/repository/StationsRepositoryImpl.kt
@@ -2,17 +2,28 @@ package com.challenge.radio.station.repository
 
 import com.challenge.radio.station.api.StationApi
 import com.challenge.radio.station.model.Station
+// TODO [Storage]
+//  For storage would use a Room database created parallel to the "api" package.
+//  and inject stationsDao here, can have one table for Stations
+//  Since the repository is the source of stations, when adding a database this file would be the only file to be modified
+//  - in addition to the database creation /DAO code.
 
 class StationsRepositoryImpl(
     private val stationApi: StationApi,
 ) : StationsRepository {
     override suspend fun loadTopStations(count: Int): Result<List<Station>> =
         runCatching {
+            // TODO [Storage]
+            //  Depending on the use case, if we prefer to load first from storage if existing
+            //  and then do the network fetching, or use storage as backup in case the api call fails
+            //  the api call would be called before or after loading stations from storage
             stationApi.listStationsBySystemName(
                 systemName = StationApi.SystemName.STATIONS_TOP,
                 count = count,
             )
         }.map { response ->
+            // TODO [Storage]
+            //  as a side effect would update the storage here with the new values
             response.playables.map {
                 it.toStation()
             }
@@ -20,6 +31,8 @@ class StationsRepositoryImpl(
 
     override suspend fun getStationsDetailsById(ids: List<String>): Result<List<Station>> =
         kotlin.runCatching {
+            // TODO [Storage]
+            //  update records with the values returned from the details api fetching
             stationApi.getStationDetailsByIds(ids = ids).map { apiStationDetail ->
                 apiStationDetail.toStation()
             }
